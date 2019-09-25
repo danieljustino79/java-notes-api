58995432eab0fa7ab48e9f60e8153aa4b11f90dd
Daniel Justino <danieljustino79@gmail.com>
2019-09-25 20:24:22 -0300

    Spring Boot 2, Spring Web, Spring Data MongoDB

---
+++ src/main/java/com/sb2/javanotesapi/JavaNotesApiApplication.java
@@ -0,0 +1,15 @@
+package com.sb2.javanotesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaNotesApiApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(JavaNotesApiApplication.class, args);
+		System.out.println("SpringBoot 2");
+	}
+
+}
---
+++ src/test/java/com/sb2/javanotesapi/JavaNotesApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sb2.javanotesapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavaNotesApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
