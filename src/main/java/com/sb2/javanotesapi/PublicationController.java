954fc4378c60b40915d8d7bb540d0d03b6e5286e
Daniel Justino <danieljustino79@gmail.com>
2019-09-25 21:08:31 -0300

    PublicationService

--- src/main/java/com/sb2/javanotesapi/PublicationController.java
+++ src/main/java/com/sb2/javanotesapi/PublicationController.java
@@ -1,5 +1,7 @@
 package com.sb2.javanotesapi;
 
+import com.sb2.javanotesapi.services.PublicationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/publications")
 public class PublicationController {
+    @Autowired
+    PublicationService service;
+
     @GetMapping
     public String get(){
-        return "SpringBoot REST..";
+        return service.gatAll();
     }
 }
---
+++ src/main/java/com/sb2/javanotesapi/services/PublicationService.java
@@ -0,0 +1,10 @@
+package com.sb2.javanotesapi.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PublicationService {
+    public String gatAll(){
+        return "SpringBoot Service";
+    }
+}
